67acf8c3936582c43e541beb56c3a1c3708e7f13
Iorgen Wildrik <w.drik@outlook.com>
2018-08-18 13:51:28 -0300

    Create and list posts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: 'http://localhost:3333'
+})
 
 class App extends Component {
+  state = {
+    newPostContent: '',
+    posts: [],
+  }
+
+  async componentDidMount() {
+    const { data: posts } = await api.get('/posts')
+
+    this.setState({ posts })
+  }
+
+  handlePostSave = async (e) => {
+    e.preventDefault()
+
+    const { data: post } = await api.post('/posts', { content: this.state.newPostContent })
+
+    this.setState({ posts: [...this.state.posts, post], newPostContent: '' })
+  }
+
   render() {
     return (
       <div className="App">
-        <textarea/>
-        <button type="submit">Postar</button>
+        <form onSubmit={ this.handlePostSave }>
+          <textarea 
+            onChange={ e => this.setState({ newPostContent: e.target.value }) }
+            value={ this.state.newPostContent } 
+          />
+          <button type="submit">Postar</button>
+        </form>
+
+        <ul>
+          { this.state.posts.map(post => (
+            <li key={ post.id }>{ post.content }</li>
+          )) }
+        </ul>
       </div>
     )
   }
