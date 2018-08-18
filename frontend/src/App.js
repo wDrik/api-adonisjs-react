9e4a7ccacf2234fb9bb958d8e475e9cb964ecc98
Iorgen Wildrik <w.drik@outlook.com>
2018-08-18 13:20:04 -0300

    Create front-end React App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <textarea/>
+        <button type="submit">Postar</button>
+      </div>
+    )
+  }
+}
+
+export default App;
