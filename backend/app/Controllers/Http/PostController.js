021b090dc9d016f7a1a4a5ecec0eefff70402a74
Iorgen Wildrik <w.drik@outlook.com>
2018-08-18 12:51:04 -0300

    Post Controller and Routes

--- backend/app/Controllers/Http/PostController.js
+++ backend/app/Controllers/Http/PostController.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const Post = use('App/Models/Post')
+
 /**
  * Resourceful controller for interacting with posts
  */
@@ -8,49 +10,56 @@ class PostController {
    * Show a list of all posts.
    * GET posts
    */
-  async index ({ request, response, view }) {
-  }
+  async index () {
+    const posts = await Post.all()
 
-  /**
-   * Render a form to be used for creating a new post.
-   * GET posts/create
-   */
-  async create ({ request, response, view }) {
+    return posts
   }
 
   /**
    * Create/save a new post.
    * POST posts
    */
-  async store ({ request, response }) {
+  async store ({ request }) {
+    const data = request.only(['content'])
+    const post = await Post.create(data)
+
+    return post
   }
 
   /**
    * Display a single post.
    * GET posts/:id
    */
-  async show ({ params, request, response, view }) {
-  }
+  async show ({ params }) {
+    const post = await Post.find(params.id)
 
-  /**
-   * Render a form to update an existing post.
-   * GET posts/:id/edit
-   */
-  async edit ({ params, request, response, view }) {
+    return post
   }
 
   /**
    * Update post details.
    * PUT or PATCH posts/:id
    */
-  async update ({ params, request, response }) {
+  async update ({ params, request }) {
+    const data = request.only(['content'])
+    const post = await Post.find(params.id)
+
+    post.merge(data)
+
+    await post.save()
+
+    return post
   }
 
   /**
    * Delete a post with id.
    * DELETE posts/:id
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params }) {
+    const post = await Post.find(params.id)
+
+    await post.delete()
   }
 }
 
